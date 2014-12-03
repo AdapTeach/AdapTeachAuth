3e9da580b311a5e546d53f4371f38bccfed7d2ed
Mikael Couzic <mikaelcouzic@gmail.com>
2014-12-03 15:12:48 +0100

    Start from scratch

--- src/config/env/example.js
+++
@@ -1,9 +0,0 @@
-(function () {
-  'use strict';
-
-  module.exports = {
-    db: 'mongodb://localhost/yourDb',
-    TOKEN_SECRET: 'yoursecrethere',
-    GITHUB_SECRET: 'XXXXXXXXXXXXXXXXX'
-  };
-})();
